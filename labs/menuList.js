37a717824d1d0f01b49fed1669b9f8ebae7dc0be
Volodymyr Myshko <v.myshko.synergetica@gmail.com>
2022-10-22 01:11:45 +0300

    feat: textAlign for menuList impl WIP

--- labs/menuList.js
+++ labs/menuList.js
@@ -1,8 +1,14 @@
-import { TextSprite } from "./textSprite.js";
+import { TextAlign, TextSprite } from "./textSprite.js";
 
 const noOp = () => console.log("noOp");
 
+const debug = false; //show debug rects
+
 export class ListItem {
+  get width() {
+    return this.text.length * this.textSprite.charSize;
+  }
+
   constructor({ text, itemColor, onSelect = noOp }) {
     this.text = text;
     this.onSelect = onSelect;
@@ -12,19 +18,30 @@ export class ListItem {
       fillStyle: itemColor,
     });
   }
+
   select() {
     this.onSelect();
   }
+
   draw(ctx, x, y) {
     this.textSprite.draw(ctx, x, y);
+
+    if (debug) {
+      ctx.strokeStyle = "magenta";
+      ctx.strokeRect(x, y, this.width, this.textSprite.charSize);
+    }
   }
 }
 
 export class ListItemSelect extends ListItem {
-  //
+  #maxOptionLength;
 
   get value() {
     return this.options[this.selectedIndex];
+  }
+
+  get width() {
+    return this.#maxOptionLength * this.textSprite.charSize + this.valueOffsetX;
   }
 
   constructor({
@@ -41,6 +58,10 @@ export class ListItemSelect extends ListItem {
     // this.value = value;
     this.selectedIndex = options.includes(value) ? options.indexOf(value) : 0;
     this.options = options;
+
+    this.#maxOptionLength = Math.max(
+      ...this.options.map((option) => `${option}`.length)
+    );
 
     this.valueOffsetX =
       valueOffsetX ?? (text.length + 1) * this.textSprite.charSize;
@@ -70,19 +91,35 @@ export class ListItemSelect extends ListItem {
     selectedOption.draw(ctx, x + this.valueOffsetX, y);
   }
 }
+
 export class MenuList {
+  #textAlignOffsetX;
+
   constructor({
     listItems,
     cursor,
     lineSpacing = 0,
     cursorOffsetX,
     selectedIndex = 0,
+    textAlign = TextAlign.left,
   }) {
     this.listItems = listItems;
     this.cursor = cursor;
     this.lineSpacing = lineSpacing;
     this.cursorOffsetX = cursorOffsetX;
     this.selectedIndex = selectedIndex;
+    this.textAlign = textAlign; // todo(vmyshko): impl
+
+    this.width = Math.max(...this.listItems.map((item) => item.width));
+
+    this.#textAlignOffsetX =
+      -1 *
+      this.width *
+      {
+        [TextAlign.left]: 0,
+        [TextAlign.center]: 0.5,
+        [TextAlign.right]: 1,
+      }[this.textAlign];
   }
 
   get selectedItem() {
@@ -108,22 +145,34 @@ export class MenuList {
   }
 
   draw(ctx, x, y) {
-    this.listItems.forEach((listItem, index) =>
+    this.listItems.forEach((listItem, index) => {
       listItem.draw(
         ctx,
-        x,
+        x + this.#textAlignOffsetX,
         y + index * (listItem.textSprite.charSize + this.lineSpacing)
-      )
-    );
+      );
+    });
+
     const cursorHeight = this.cursor.sHeight;
     const itemHeight = this.selectedItem.textSprite.charSize;
 
     this.cursor.draw(
       ctx,
-      x - this.cursorOffsetX,
+      x - this.cursorOffsetX + this.#textAlignOffsetX,
       y +
         this.selectedIndex * (itemHeight + this.lineSpacing) +
         (itemHeight - cursorHeight) / 2
     );
+
+    if (debug) {
+      ctx.strokeStyle = "greenyellow";
+      ctx.strokeRect(
+        x + +this.#textAlignOffsetX,
+        y,
+        this.width,
+        (this.selectedItem.textSprite.charSize + this.lineSpacing) *
+          this.listItems.length
+      );
+    }
   }
 }
