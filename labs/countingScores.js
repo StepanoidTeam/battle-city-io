d2beba02094ff0d407d353527e9668e6db00ff11
SvetlanaVaskiv <svetanochka@gmail.com>
2022-10-23 21:59:50 +0200

    refac: add layout to scores

--- labs/countingScores.js
+++ labs/countingScores.js
@@ -1,5 +1,5 @@
-import { ListItem, MenuList } from "./menuList.js";
-import { nesWidth } from "./script.js";
+import { blackColour, gingerColour, redColour } from "./consts.js";
+import { nesWidth } from "./consts.js";
 import {
   enemyTank1,
   enemyTank2,
@@ -8,27 +8,49 @@ import {
 } from "./sprite-lib.js";
 import { TextAlign, TextSprite } from "./textSprite.js";
 
-const p1tanksDestroyed = [20, 4, 0, 2];
+const p1tanksDestroyed = [2, 4, 0, 2];
 const p2tanksDestroyed = [0, 16, 8, 5];
 const levelValue = 3;
 const tankPrices = [100, 200, 300, 400];
 const enemyTanks = [enemyTank1, enemyTank2, enemyTank3, enemyTank4];
+let p1totalScores = 0;
+let p2totalScores = 0;
+const initialPointOfView = 16;
+for (let i = 0; i < p1tanksDestroyed.length; i++) {
+  p1totalScores += p1tanksDestroyed[i] * tankPrices[i];
+  p2totalScores += p2tanksDestroyed[i] * tankPrices[i];
+}
 
 export function initCountingScores({ onStartGame }) {
   const hightScores = new TextSprite({
     text: "hi-score",
-    fillStyle: "rgb(263,154,69)",
+    fillStyle: `${gingerColour}`,
   });
   const averageScores = new TextSprite({
     text: "45000",
-    fillStyle: "red",
+    fillStyle: `${redColour}`,
   });
   const level = new TextSprite({
     text: `stage ${levelValue}`,
   });
   const onePlayer = new TextSprite({
-    text: `I-player II-player\n`,
-    fillStyle: "red",
+    text: `I-player`,
+    fillStyle: `${redColour}`,
+    textAlign: TextAlign.right,
+  });
+  const twoPlayers = new TextSprite({
+    text: `II-player`,
+    fillStyle: `${redColour}`,
+    textAlign: TextAlign.right,
+  });
+  const onePlayerScores = new TextSprite({
+    text: `${p1totalScores}`,
+    fillStyle: `${gingerColour}`,
+    textAlign: TextAlign.right,
+  });
+  const secondPlayerScores = new TextSprite({
+    text: `${p2totalScores}`,
+    fillStyle: `${gingerColour}`,
     textAlign: TextAlign.right,
   });
   const p1totalAmountKilledTanks = p1tanksDestroyed.reduce(
@@ -42,10 +64,19 @@ export function initCountingScores({ onStartGame }) {
   const totalLine = new TextSprite({
     text: `${"".padStart(8, "_")}\ntotal ${p1totalAmountKilledTanks
       .toString()
-      .padStart(2)}${p2totalAmountKilledTanks.toString().padStart(2+4)}`,
+      .padStart(2)}${p2totalAmountKilledTanks.toString().padStart(2 + 4)}`,
     textAlign: TextAlign.right,
   });
 
+  const bonus = new TextSprite({
+    text: "bonus!",
+    textAlign: TextAlign.right,
+    fillStyle: `${redColour}`,
+  });
+  const bonusPoints = new TextSprite({
+    text: "1000 pts",
+    textAlign: TextAlign.right,
+  });
   const p1score = new TextSprite({
     text: p1tanksDestroyed
       .map(
@@ -87,24 +118,47 @@ export function initCountingScores({ onStartGame }) {
   return {
     draw(ctx) {
       //bg
-      ctx.fillStyle = "black";
+      ctx.fillStyle = `${blackColour}`;
       ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
       // title
-      hightScores.draw(ctx, 64, 14);
-      averageScores.draw(ctx, 158, 14);
-      level.draw(ctx, 100, 40);
-      onePlayer.draw(ctx, nesWidth / 2 - 4 * 8 - 8, 55);
-      p1score.draw(ctx, nesWidth / 2 - 8, 100);
+      hightScores.draw(ctx, 64, initialPointOfView);
+      averageScores.draw(ctx, 158, initialPointOfView);
+      level.draw(ctx, 100, initialPointOfView * 2);
+      onePlayer.draw(ctx, nesWidth / 2 - 4 * 8 - 8, initialPointOfView * 3);
+      onePlayerScores.draw(
+        ctx,
+        nesWidth / 2 - 4 * 8 - 8,
+        initialPointOfView * 4
+      );
+      twoPlayers.draw(ctx, nesWidth / 2 + 12 * 8 + 8, initialPointOfView * 3);
+      secondPlayerScores.draw(
+        ctx,
+        nesWidth / 2 + 12 * 8 + 8,
+        initialPointOfView * 4
+      );
+
+      p1score.draw(ctx, nesWidth / 2 - 8, initialPointOfView * 5);
       enemyTanks.forEach((tank, index) =>
         tank.draw(
           ctx,
           nesWidth / 2 - 8 + 1,
-          100 + index * (tank.sHeight + 8) - 4
+          initialPointOfView * 5 + index * (tank.sHeight + 8) - 4
         )
       );
-      p2score.draw(ctx, nesWidth / 2 + 12 * 8 + 8, 100);
-      totalLine.draw(ctx, nesWidth / 2 + 8*4, 100 + (8 * 4 + 16 * 3));
+      p2score.draw(ctx, nesWidth / 2 + 12 * 8 + 8, initialPointOfView * 5);
+      totalLine.draw(
+        ctx,
+        nesWidth / 2 + 8 * 4,
+        initialPointOfView * 5 + (8 * 4 + 16 * 3)
+      );
+      p1totalAmountKilledTanks >= 20
+        ? (bonus.draw(ctx, nesWidth / 2 - 5 * 8, initialPointOfView * 12),
+          bonusPoints.draw(ctx, nesWidth / 2 - 5 * 8, initialPointOfView * 13))
+        : p2totalAmountKilledTanks >= 20
+        ? (bonus.draw(ctx, nesWidth / 2 + 8 * 13, initialPointOfView * 12),
+          bonusPoints.draw(ctx, nesWidth / 2 + 8 * 13, initialPointOfView * 13))
+        : null;
       // draw list
     },
     load() {
