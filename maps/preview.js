a5b8ed0b208e9b06fe1aa6bf9284cda383b822b0
Pavlo Mykhalov <mykhalov@google.com>
2022-10-25 22:22:38 +0300

    Add a PNG loader

--- maps/preview.js
+++ maps/preview.js
@@ -1,32 +1,4 @@
-const tiles = {
-  Void: 0x00,
-  Forest: 0x0f,
-  Concrete: 0x10,
-  Water: 0x12,
-  Ice: 0x21,
-  Brick: 0x22,
-};
-
-const colors = {
-  Black: [0, 0, 0, 255],
-  Green: [138, 76, 10, 255],
-  DarkGray: [127, 127, 127, 255],
-  Blue: [67, 67, 247, 255],
-  LightGray: [172, 172, 172, 255],
-  Brown: [215, 255, 72, 255],
-  Magenta: [255, 0, 255, 255],
-};
-
-const tileColors = new Map([
-  [tiles.Void, colors.Black],
-  [tiles.Forest, colors.Green],
-  [tiles.Concrete, colors.DarkGray],
-  [tiles.Water, colors.Blue],
-  [tiles.Ice, colors.LightGray],
-  [tiles.Brick, colors.Brown],
-]);
-
-const FallbackColor = colors.Magenta;
+import { tileColor } from "./tileColors.js";
 
 export function preview(maps) {
   for (const [index, map] of maps.entries()) {
@@ -50,11 +22,10 @@ function createCanvas(map) {
 }
 
 function putImageData(canvas, map) {
-  const data = new Uint8ClampedArray(
-    map.tiles.flatMap((tile) => tileColors.get(tile) ?? FallbackColor)
-  );
+  const data = new Uint8ClampedArray(map.tiles.flatMap(tileColor));
+  const imageData = new ImageData(data, map.width);
 
-  canvas.getContext("2d").putImageData(new ImageData(data, map.width), 0, 0);
+  canvas.getContext("2d").putImageData(imageData, 0, 0);
 }
 
 function createDownloadLink(canvas, index) {
