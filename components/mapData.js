00ad7978b39d84fe8c1c09717e3770bdebc1eeeb
Volodymyr Myshko <v.myshko.synergetica@gmail.com>
2022-10-30 19:04:57 +0200

    refac: safe getTileId impl, forEachTile() loop helper impl

--- components/mapData.js
+++ components/mapData.js
@@ -2,6 +2,20 @@ import { fieldOffsetX, fieldOffsetY, fragmentSize, tiles } from "../consts.js";
 import { tileSprites } from "./sprite-lib.js";
 
 export class MapData {
+  /**
+   * safely get matrix value
+   * @param {*} col
+   * @param {*} row
+   * @returns tileId, or null - if out of bounds
+   */
+  getTileId(col, row) {
+    const column = this.fieldMatrix[col];
+
+    if (!column) return null;
+
+    return column[row] ?? null;
+  }
+
   fieldMatrix = null;
   constructor({ cols, rows }) {
     this.cols = cols;
@@ -24,6 +38,19 @@ export class MapData {
   }
 }
 
+function forEachTile({ mapData, callback }) {
+  const { cols, rows, fieldMatrix } = mapData;
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+      const tileId = fieldMatrix[row][col];
+
+      callback(tileId, row, col);
+    }
+  }
+}
+
+export const pathlessBlocks = [null, tiles.Brick, tiles.Concrete, tiles.Water];
+
 export class MapDrawer {
   constructor({ mapData }) {
     this.mapData = mapData;
@@ -32,14 +59,12 @@ export class MapDrawer {
   update(timestamp) {}
 
   #drawField(ctx) {
-    const { cols, rows, fieldMatrix } = this.mapData;
-    for (let row = 0; row < rows; row++) {
-      for (let col = 0; col < cols; col++) {
-        const tileId = fieldMatrix[row][col];
-
+    forEachTile({
+      mapData: this.mapData,
+      callback: (tileId, row, col) => {
         const tileSprite = tileSprites.get(tileId);
 
-        if (!tileSprite) continue;
+        if (!tileSprite) return;
         tileSprite.draw(
           ctx,
           row * fragmentSize + fieldOffsetX,
@@ -47,11 +72,36 @@ export class MapDrawer {
           fragmentSize,
           fragmentSize
         );
-      }
-    }
+      },
+    });
+  }
+
+  #drawColliders(ctx) {
+    if (globalThis.debug === false) return;
+    forEachTile({
+      mapData: this.mapData,
+      callback: (tileId, row, col) => {
+        if (!pathlessBlocks.includes(tileId)) return;
+        ctx.strokeStyle = "magenta";
+        ctx.strokeRect(
+          row * fragmentSize + fieldOffsetX,
+          col * fragmentSize + fieldOffsetY,
+          fragmentSize,
+          fragmentSize
+        );
+        ctx.fillStyle = "rgba(255,0,255,0.2)";
+        ctx.fillRect(
+          row * fragmentSize + fieldOffsetX,
+          col * fragmentSize + fieldOffsetY,
+          fragmentSize,
+          fragmentSize
+        );
+      },
+    });
   }
 
   draw(ctx) {
     this.#drawField(ctx);
+    this.#drawColliders(ctx);
   }
 }
